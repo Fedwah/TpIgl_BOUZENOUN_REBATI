2602bd771b65d35ec79516b4fecd76dd1f8019aa
Rebati Roumaissa <fr_rebati@esi.dz>
2017-10-14 17:12:08 +0100

    Ajout de sous repertoire

--- TP1_IGL/Program.cs
+++ TP1_IGL/Program.cs
@@ -10,6 +10,31 @@ namespace TP1_IGL
     {
         static void Main(string[] args)
         {
+            int[] T1 = new int[20] { 5,4,1,9,10,600, 8,17, 33,92, 55, 54, 91, 99, 710, -6, 80, 7, 3, 2 };
+            int[] T2 = new int[20];
+            VectorHelper v = new VectorHelper();
+            v.TRierVecteur(T1);
+         
+            for(int i=0;i<T1.Length;i++)
+            {
+                Console.WriteLine(T1[i]);
+            }
+            v.ADdn(T1, 5);
+            for (int i = 0; i < T1.Length; i++)
+            {
+                Console.WriteLine(T1[i]);
+            }
+            v.MUltipparn(T1, 2);
+            for (int i = 0; i < T1.Length; i++)
+            {
+                Console.WriteLine(T1[i]);
+            }
+            v.SOustrairen(T1, 4);
+            for (int i = 0; i < T1.Length; i++)
+            {
+                Console.WriteLine(T1[i]);
+            }
+
         }
     }
 }
---
+++ TP1_IGL/VectorHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_IGL
+{
+    class VectorHelper
+    {
+       // int[] elements;
+        /*int TAille;*/
+        public VectorHelper()
+        {
+           // elements = new int[100];
+
+        }
+        public void TRierVecteur(int[] vecteuratrier)//Tri par bulle
+        {
+          
+            int i;bool perm;int inter;
+            do
+            {
+                perm = false;
+                
+                for (i = 0; i < vecteuratrier.Length-1 ; i++)
+                {
+                    if (vecteuratrier[i] > vecteuratrier[i + 1])
+                    {
+                        inter = vecteuratrier[i];
+                        vecteuratrier[i] = vecteuratrier[i + 1];
+                        vecteuratrier[i + 1] = inter;
+                        perm = true;
+                    }
+
+                }
+            } while (perm);
+
+        }
+        public void MUltipparn(int[] vecteuratriter, int n)
+        {
+            for(int i=0;i<vecteuratriter.Length;i++)
+            {
+                vecteuratriter[i] = vecteuratriter[i] * n;
+            }
+        }
+        public void ADdn(int[] vecteuratriter,int n)
+        {
+            for (int i = 0; i < vecteuratriter.Length; i++)
+            {
+                vecteuratriter[i] = vecteuratriter[i] + n;
+            }
+        }
+        public void SOustrairen(int[] vecteuratriter,int n)
+        {
+            for (int i = 0; i < vecteuratriter.Length; i++)
+            {
+                vecteuratriter[i] = vecteuratriter[i] - n;
+            }
+        }
+            /* public int[] getElements()
+              {
+                 return elements; 
+              }    */
+        }
+}
