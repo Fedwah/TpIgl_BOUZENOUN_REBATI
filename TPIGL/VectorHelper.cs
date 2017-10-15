7a51b221f7e1ef082228ac42ca36c82fd90976ff
fedwa <ff_bouzenoun@esi.dz>
2017-10-15 16:36:30 +0100

    roumaissa versions

--- TPIGL/VectorHelper.cs
+++ TPIGL/VectorHelper.cs
@@ -65,5 +65,55 @@ namespace TPIGL
                 if (table[i] < min) min = table[i];
             }
         }
+        public void TRierVecteur(int[] vecteuratrier)//Tri par bulle
+        {
+
+            int i; bool perm; int inter;
+            do
+            {
+                perm = false;
+
+                for (i = 0; i < vecteuratrier.Length - 1; i++)
+                {
+                    if (vecteuratrier[i] > vecteuratrier[i + 1])
+                    {
+                        inter = vecteuratrier[i];
+                        vecteuratrier[i] = vecteuratrier[i + 1];
+                        vecteuratrier[i + 1] = inter;
+                        perm = true;
+                    }
+
+                }
+            } while (perm);
+
+        }
+        public void MUltipparn(int[] vecteuratriter, int n)
+        {
+            for (int i = 0; i < vecteuratriter.Length; i++)
+            {
+                vecteuratriter[i] = vecteuratriter[i] * n;
+            }
+        }
+        public void ADdn(int[] vecteuratriter, int n)
+        {
+            for (int i = 0; i < vecteuratriter.Length; i++)
+            {
+                vecteuratriter[i] = vecteuratriter[i] + n;
+            }
+        }
+        public void SOustrairen(int[] vecteuratriter, int n)
+        {
+            for (int i = 0; i < vecteuratriter.Length; i++)
+            {
+                vecteuratriter[i] = vecteuratriter[i] - n;
+            }
+        }
+        public void Opposee(int[] vecteuratriter)
+        {
+            for (int i = 0; i < vecteuratriter.Length; i++)
+            {
+                vecteuratriter[i] = vecteuratriter[i] * -1;
+            }
+        }
     }
 }
