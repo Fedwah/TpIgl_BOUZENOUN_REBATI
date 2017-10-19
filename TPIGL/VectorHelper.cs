01cb225ea736aeb403b954ae1e3492bb2f35957d
Rebati Roumaissa <fr_rebati@esi.dz>
2017-10-19 12:06:53 +0100

    Rename of ome files to facilate the merge

--- TPIGL/VectorHelper.cs
+++ TPIGL/VectorHelper.cs
@@ -4,67 +4,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TPIGL
+namespace TP1_IGL
 {
     class VectorHelper
     {
-        public int[] Som2Vect(int[] vect1, int[] vect2)
+      
+        public VectorHelper()
         {
-            int[] som = null;
-            try
+          
+
+        }
+        public void TRierVecteur(int[] vecteuratrier)//Tri par bulle
+        {
+          
+            int i;bool perm;int inter;
+            do
             {
-
-                if (vect1.Length == vect2.Length)
+                perm = false;
+                
+                for (i = 0; i < vecteuratrier.Length-1 ; i++)
                 {
-                    som = new int[vect1.Length];
-                    for (int i = 0; i < vect1.Length; i++)
+                    if (vecteuratrier[i] > vecteuratrier[i + 1])
                     {
-
-                        som[i] = (int)vect1.GetValue(i) + (int)vect2.GetValue(i);
-
+                        inter = vecteuratrier[i];
+                        vecteuratrier[i] = vecteuratrier[i + 1];
+                        vecteuratrier[i + 1] = inter;
+                        perm = true;
                     }
 
                 }
-                else
-                {
-                    throw new exepTai();
-                }
-            }
-            catch (exepTai e)
-            {
-                Console.WriteLine("Les deux tableaux n ont pas la meme taille, \nvous ne pouvez pas effectuer cette operation");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("un des tableaux n existe pas");
-            }
-            return som;
+            } while (perm);
+
         }
-        public void swap(int v1,int v2)
-            {
-            int permute = v2;
-            v2 = v1;
-            v1 = permute;
-            }
-        public int[] reverse(int[] table)
+        public void MUltipparn(int[] vecteuratriter, int n)
         {
-            int start, end;
-            for (start = 0,end = table.Length;start < (table.Length / 2);start++,end--)
+            for(int i=0;i<vecteuratriter.Length;i++)
             {
-                swap(table[start], table[end]);
+                vecteuratriter[i] = vecteuratriter[i] * n;
             }
-            return table;
         }
-        public  void maxMinOfTable(int[] table,int max,int min)
+        public void ADdn(int[] vecteuratriter,int n)
         {
-            max = table[0];
-            min = table[0];
-            for(int i=1;i<table.Length;i++)
+            for (int i = 0; i < vecteuratriter.Length; i++)
             {
-                if (table[i] > max) max = table[i];
-                if (table[i] < min) min = table[i];
+                vecteuratriter[i] = vecteuratriter[i] + n;
             }
         }
-       
+        public void SOustrairen(int[] vecteuratriter,int n)
+        {
+            for (int i = 0; i < vecteuratriter.Length; i++)
+            {
+                vecteuratriter[i] = vecteuratriter[i] - n;
+            }
+        }
+        public void Opposee(int [] vecteuratriter)
+        {
+            for (int i = 0; i < vecteuratriter.Length; i++)
+            {
+                vecteuratriter[i] = vecteuratriter[i] * -1;
+            }
+        }
+
     }
 }
