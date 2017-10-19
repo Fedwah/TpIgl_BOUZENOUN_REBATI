112b0000bbdddec06db7999f84e59393b5775960
Rebati Roumaissa <fr_rebati@esi.dz>
2017-10-19 13:38:57 +0100

    adding VectorHelper qui contient le fusion de nos parties

--- TPIGL/VectorHelper.cs
+++ TPIGL/VectorHelper.cs
@@ -6,23 +6,21 @@ using System.Threading.Tasks;
 
 namespace TPIGL
 {
-    class VectorHelper
+    public class VectorHelper
     {
-      
         public VectorHelper()
         {
-          
 
         }
         public void TRierVecteur(int[] vecteuratrier)//Tri par bulle
         {
-          
-            int i;bool perm;int inter;
+
+            int i; bool perm; int inter;
             do
             {
                 perm = false;
-                
-                for (i = 0; i < vecteuratrier.Length-1 ; i++)
+
+                for (i = 0; i < vecteuratrier.Length - 1; i++)
                 {
                     if (vecteuratrier[i] > vecteuratrier[i + 1])
                     {
@@ -38,26 +36,26 @@ namespace TPIGL
         }
         public void MUltipparn(int[] vecteuratriter, int n)
         {
-            for(int i=0;i<vecteuratriter.Length;i++)
+            for (int i = 0; i < vecteuratriter.Length; i++)
             {
                 vecteuratriter[i] = vecteuratriter[i] * n;
             }
         }
-        public void ADdn(int[] vecteuratriter,int n)
+        public void ADdn(int[] vecteuratriter, int n)
         {
             for (int i = 0; i < vecteuratriter.Length; i++)
             {
                 vecteuratriter[i] = vecteuratriter[i] + n;
             }
         }
-        public void SOustrairen(int[] vecteuratriter,int n)
+        public void SOustrairen(int[] vecteuratriter, int n)
         {
             for (int i = 0; i < vecteuratriter.Length; i++)
             {
                 vecteuratriter[i] = vecteuratriter[i] - n;
             }
         }
-        public void Opposee(int [] vecteuratriter)
+        public void Opposee(int[] vecteuratriter)
         {
             for (int i = 0; i < vecteuratriter.Length; i++)
             {
@@ -65,5 +63,62 @@ namespace TPIGL
             }
         }
 
+        public int[] Som2Vect(int[] vect1, int[] vect2)
+        {
+            int[] som = null;
+            try
+            {
+
+                if (vect1.Length == vect2.Length)
+                {
+                    som = new int[vect1.Length];
+                    for (int i = 0; i < vect1.Length; i++)
+                    {
+
+                        som[i] = (int)vect1.GetValue(i) + (int)vect2.GetValue(i);
+
+                    }
+
+                }
+                else
+                {
+                    throw new exepTai();
+                }
+            }
+            catch (exepTai e)
+            {
+                Console.WriteLine("Les deux tableaux n ont pas la meme taille, \nvous ne pouvez pas effectuer cette operation");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("un des tableaux n existe pas");
+            }
+            return som;
+        }
+        public void swap(int v1,int v2)
+            {
+            int permute = v2;
+            v2 = v1;
+            v1 = permute;
+            }
+        public int[] reverse(int[] table)
+        {
+            int start, end;
+            for (start = 0,end = table.Length;start < (table.Length / 2);start++,end--)
+            {
+                swap(table[start], table[end]);
+            }
+            return table;
+        }
+        public  void maxMinOfTable(int[] table,int max,int min)
+        {
+            max = table[0];
+            min = table[0];
+            for(int i=1;i<table.Length;i++)
+            {
+                if (table[i] > max) max = table[i];
+                if (table[i] < min) min = table[i];
+            }
+        }
     }
 }
