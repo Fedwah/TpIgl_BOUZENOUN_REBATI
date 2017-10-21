4c2a828444e35da89b6a75314bc1a3c68bc9e5f3
fedwah <ff_bouzenoun@esi.dz>
2017-10-21 15:35:47 +0100

    documentation2

--- tpIGL/Program.cs
+++ tpIGL/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace tpIGL
 {
+    /// <summary>
+    /// Program contient la methode main
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
